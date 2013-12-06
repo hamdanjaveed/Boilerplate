64ced7aab5fc5d492cf65ce669c5b65e51548cc9
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-12-06 14:54:26 -0500

    Worked

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ define(function(require, exports, module) {
     var CommandManager = brackets.getModule("command/CommandManager");
     var Menus = brackets.getModule("command/Menus");
     var EditorManager = brackets.getModule("editor/EditorManager");
+    var KeyEvent = brackets.getModule("utils/KeyEvent");
     
     // the unique identifiers used to trigger actions
     var ThreeJSCommandID = "hamdanjaveed.snippets.threejs";
@@ -26,14 +27,36 @@ define(function(require, exports, module) {
         }
     }
     
+    // handle a key event
+    function handleKeyEvent($event, editor, event) {
+        if ((event.type === "keydown") && (event.keyCode === KeyEvent.DOM_VK_TAB)) {
+            console.log("Tab pressed!");
+        }
+    }
+    
     // when brackets has finished loading ...
     AppInit.appReady(function() {
         // ... register the threejs menu command
         CommandManager.register("Three JS", ThreeJSCommandID, insertThreeJSSnip);
+        
+        var editor = EditorManager.getFocusedEditor();
+        $(editor).on("keyEvent", handleKeyEvent);
+        $(EditorManager).on("activeEditorChange", editorDidChange);
         
         // add a 'Snippets' menu into the main menu bar
         var snippetsMenu = Menus.addMenu("Snippets", "hamdanjaveed.snippets");
         // add a threejs menu item with a keyboard shortcut
         snippetsMenu.addMenuItem(ThreeJSCommandID, {"key":"Ctrl-3"});
     });
+    
+    // handle an editor switch (when switching between files)
+    function editorDidChange($event, focusedEditor, lostEditor) {
+        if (lostEditor) {
+            $(lostEditor).off("keyEvent", handleKeyEvent);
+        }
+        
+        if (focusedEditor) {
+            $(focusedEditor).on("keyEvent", handleKeyEvent);
+        }
+    }
 });
